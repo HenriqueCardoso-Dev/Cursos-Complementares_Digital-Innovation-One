8f0c6e4194b1579c851871052f9bea75d54e1b2f
Carlos Henrique Ramos Cardoso <63278045+HenriqueCardoso-Dev@users.noreply.github.com>
2020-06-16 18:02:41 -0300

    primeiras aulas JS

---
+++ JavaScript/Array/arrayScript.js
@@ -0,0 +1,9 @@
+let fruitList = ['Apple', 'Orange', 'Strawberry', 'grape'];
+
+console.log(fruitList);
+
+fruitList.push('Watermelon');
+
+console.log(fruitList);
+
+fruitList.pop;
---
+++ JavaScript/introduction/_scripts/main.js
@@ -0,0 +1,22 @@
+
+let nameValue = 'Carlos Henrique Ramos Cardoso'; //String
+
+let ageValue = '19'; //String
+
+let ageValue2 = 10; //integer
+
+var textValue = 'Japan is the best team of the world!'; // text output.
+
+alert(`${nameValue} tem ${ageValue} anos de idade!`); //Data Output
+
+alert( ageValue + ageValue2); // Second Data Output
+
+console.log(nameValue); //output on console.
+console.log(ageValue + ageValue2); //outpout on console.
+
+alert(textValue.replace('Japan', 'Brazil')); //output different (mudança de caracteres na saída da variável utilizando o método replace).
+
+console.log(textValue.toUpperCase()); // transformando todo o conteúdo da variável em caracteres minúsculos na hora de sua saída.
+console.log(textValue.toLowerCase()); // transformando todo o conteúdo da variável em caracteres minúsculos na hora de sua saída.
+
+
